10f12cacf332f463d4403941c8338945f0e16e45
sche0149 <91359194+sche0149@users.noreply.github.com>
2021-10-10 15:36:10 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+let viz = 'aus_bushfire_vis.json';
+
+vegaEmbed('#vis1', viz, { "actions": false }).then(function (result) {
+
+}).catch(console.error);
+
+let viz1 = 'hw5_mapvis.json';
+
+vegaEmbed('#vis2', viz1, { "actions": false }).then(function (result) {
+
+}).catch(console.error);
+
+
